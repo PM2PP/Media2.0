0a474df0798bc2a76baf7a5cbac75689ebe6c4bd
Philip <Philip@141.22.89.13>
2018-05-07 14:01:45 +0200

    While-Schleifen für Konsolen- bzw. PC-Videospiel

--- src/PCVideospiel.java
+++ src/PCVideospiel.java
@@ -7,28 +7,38 @@ class PCVideospiel extends AbstractVideospiel
 		super(titel, kommentar, system);
 	}
 
+//	public int getPreisNachTagen(int tage)
+//	{
+//		int preis = 0;
+//
+//		if (tage > 7)
+//		{
+//			if ((tage % 4) == 0)
+//			{
+//				preis = (tage / 4 - 1) * 500;
+//			}
+//			else if ((tage % 4) == 1)
+//			{
+//				preis = (((tage - 1) / 4) - 1) * 500;
+//			}
+//			else if ((tage % 4) == 2)
+//			{
+//				preis = (((tage - 2) / 4) - 1) * 500;
+//			}
+//			else
+//			{
+//				preis = (((tage - 3) / 4) - 1) * 500;
+//			}
+//		}
+//		return preis;
+//	}
 	public int getPreisNachTagen(int tage)
 	{
 		int preis = 0;
-
-		if (tage > 7)
+		while(tage > 7)
 		{
-			if ((tage % 4) == 0)
-			{
-				preis = (tage / 4 - 1) * 500;
-			}
-			else if ((tage % 4) == 1)
-			{
-				preis = (((tage - 1) / 4) - 1) * 500;
-			}
-			else if ((tage % 4) == 2)
-			{
-				preis = (((tage - 2) / 4) - 1) * 500;
-			}
-			else
-			{
-				preis = (((tage - 3) / 4) - 1) * 500;
-			}
+			preis += 500;
+			tage -= 4;
 		}
 		return preis;
 	}
