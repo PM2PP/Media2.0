77775e6f2fcd6e621b962c4bb91cea7669fd0e38
PRitter <PRitter@ws-69-68.haw-1x.haw-hamburg.de>
2018-05-08 19:11:11 +0200

    ...

--- src/VerleihProtokollierer.java
+++ src/VerleihProtokollierer.java
@@ -4,29 +4,34 @@ import java.io.IOException;
 public class VerleihProtokollierer
 {
 	FileWriter _filewriter;
-	
+
 	public VerleihProtokollierer()
-	{ 
+	{
 		try
 		{
-			//Beispiele für Fehler, wenn ungültiger Pfad statt "./protokoll.txt" angegeben wird:
-			//C:\\Users\\Philip\\Documents -> dafür z.B. Zugriff verweigert, da ungeschützt
-			//D:\\ -> Laufwerk D existiert bei Surface nicht, daher ebenfalls Fehler
-			//Anmerkung: Zwei Backslash in einem String, also "\\" stehen für einen Backslash \
-			_filewriter = new FileWriter("./protokoll.txt", true); //false falls beim ersten Mal alles überschrieben werden soll
+			// Beispiele für Fehler, wenn ungültiger Pfad statt "./protokoll.txt" angegeben
+			// wird:
+			// C:\\Users\\Philip\\Documents -> dafür z.B. Zugriff verweigert, da ungeschützt
+			// D:\\ -> Laufwerk D existiert bei Surface nicht, daher ebenfalls Fehler
+			// Anmerkung: Zwei Backslash in einem String, also "\\" stehen für einen
+			// Backslash \
+			_filewriter = new FileWriter("./protokoll.", true); // false falls beim ersten Mal alles überschrieben
+										 						// werden soll
 		}
 		catch (IOException e)
 		{
 			System.err.println(e);
-//			e.printStackTrace();
+			// e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Speichert einen Verleihvorgang.
 	 * 
-	 * @param ereignis Hält den Verleihvorgang fest (Ausleihe oder Rückgabe).
-	 * @param verleihkarte Die Verleihkarte zum zugehörigen Medium, welches ausgeliehen wird.
+	 * @param ereignis
+	 *            Hält den Verleihvorgang fest (Ausleihe oder Rückgabe).
+	 * @param verleihkarte
+	 *            Die Verleihkarte zum zugehörigen Medium, welches ausgeliehen wird.
 	 * 
 	 * @require ereignis != null
 	 * @require ereignis.equals("Ausleihe") || ereignis.equals("Rückgabe")
@@ -35,21 +40,20 @@ public class VerleihProtokollierer
 	public void protokolliere(String ereignis, Verleihkarte verleihkarte) throws ProtokollierException
 	{
 		assert ereignis != null : "Vorbedingung verletzt: ereignis != null";
-		assert ereignis.equals("Ausleihe") || ereignis.equals("Rückgabe") : "Vorbedingung verletzt: ereignis.equals(\"Ausleihe\") || ereignis.equals(\"Rückgabe\")";
+		assert ereignis.equals("Ausleihe") || ereignis.equals(
+				"Rückgabe") : "Vorbedingung verletzt: ereignis.equals(\"Ausleihe\") || ereignis.equals(\"Rückgabe\")";
 		assert verleihkarte != null : "Vorbedingung verletzt: verleihkarte != null";
-			
-		try(FileWriter filewriter = new FileWriter("./protokoll.txt", true))
+
+		try (FileWriter filewriter = new FileWriter("./protokoll.txt", true))
 		{
-			
 			filewriter.write(ereignis + ": " + verleihkarte.getFormatiertenString());
 			//filewriter.close();
-//			_filewriter = new FileWriter("./protokoll.txt", true);
+			// _filewriter = new FileWriter("./protokoll.txt", true);
 		}
 		catch (IOException e)
 		{
 			throw new ProtokollierException(e.toString());
-//			System.err.println(e);
-//			e.printStackTrace();
+			// System.err.println(e);
 		}
 	}
 
